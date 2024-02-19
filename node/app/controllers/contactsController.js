c2d87a2b4974d9754d6be07cdcdb9cebc13ab746
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-02-18 21:09:54 -0600

    add get by id code

--- node/app/controllers/contactsController.js
+++ node/app/controllers/contactsController.js
@@ -1,11 +1,14 @@
 const DATA = require("@jworkman-fs/asl/src/Data/contacts");
-
+const { InvalidEnumError, InvalidOperatorError, InvalidContactResourceError, DuplicateContactResourceError, InvalidContactSchemaError } = require("@jworkman-fs/asl/src/Exception/index");
+const { sortContacts, filterContacts, ContactModel, validateContactData } = require("@jworkman-fs/asl/src/index");
 
 const getAllContacts = (req,res) =>{
     res.status(200).json(DATA)
 }
 const getContactById = (req,res) =>{
-    res.status(200).json(DATA)
+    const {id} = req.params;
+    const contact = ContactModel.show(id);
+    res.status(200).json(contact);
 }
 const createContact = (req,res) =>{
     res.status(200).json(DATA)
