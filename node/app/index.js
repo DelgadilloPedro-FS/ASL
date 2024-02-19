736bffe6df15e51ae30e0c0e53f6a623d1240f4c
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-02-18 20:01:22 -0600

    docker init

---
+++ node/app/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+//const routeHandler = require("./routes");
+const morgan = require("morgan");
+
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "server is running", success: true });
+});
+
+
+//app.use("/contacts", routeHandler);
+
+module.exports = app;
