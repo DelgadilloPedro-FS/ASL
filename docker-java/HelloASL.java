912aaca7b71eafcac2aedd3c1badb2c663814dab
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-02-25 18:23:15 -0600

    blank init

--- docker-java/HelloASL.java
+++
@@ -1,9 +0,0 @@
-public class HelloASL {
-
-    public static void main(String[] args) {
-        String greeting = "Hello ASL!";
-        String date = java.time.LocalDate.now().toString();
-
-        System.out.println(greeting + " Today's date is: " + date);
-    }
-}
